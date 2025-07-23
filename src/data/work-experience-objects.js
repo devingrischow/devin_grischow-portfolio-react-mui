dd9680a038b2dca782ace52fae55b3104d7c8e92
devingrischow <devingrischow@outlook.com>
2025-07-23 11:20:49 -0500

    Improved more text changes.

--- src/data/work-experience-objects.js
+++ src/data/work-experience-objects.js
@@ -18,8 +18,9 @@ export const workExperience = [
         workTime:"Jun 2024 - Aug 2024",
         workType:"Contract - Remote",
         points:[
-            "Tested a variety of data entry, visualization, and image detection software before the program like Orange, an Apple AI Model Prototyping app, and Xcode’s AI Model creator.",
-            "Aided in helping students grasp modern AI like the different model types, their applications, use cases, and more. Students then took the knowledge and each built AI-powered apps to solve problems they thought AI would be helpful with."
+            "Tested a variety of data entry, visualization, and image detection software before the program like Orange, and Apple AI Model creation tools.",
+            "Aided in helping students grasp modern AI models types, their applications, use cases, and more.",
+            "Students then took the knowledge and each built AI-powered apps to solve problems they thought AI would be helpful with."
         ]
     },
 
@@ -29,8 +30,8 @@ export const workExperience = [
         workTime:"Jan 2024 - April 2024",
         workType:"Contract - Remote",
         points:[
-            "Tutored over 2 teams with 10 beginner developers, helping them learn to plan, design, create, and publish SwiftUI apps onto the iOS App Store, while working with a client’s needs.",
-            "Teams learned to take concepts and requests for features like trying to get insights into and connecting Chicago communities, and to successfully deliver apps and ideas according to the requirements."
+            "Tutored over 2 teams with 10 beginner developers, helping them learn to plan, design, create, and publish iOS apps, according to a client’s needs.",
+            "Teams learned to take concepts and requests for features like trying to get insights into and connecting Chicago communities, and to successfully deliver apps and ideas according to those requirements."
         ]
     },
 
@@ -41,7 +42,7 @@ export const workExperience = [
         workTime:"Sep 2023 - Dec 2023",
         workType:"Contract - Remote",
         points:[
-            "Provided teaching & tutoring assistance to 17 students to help develop & publish 5 SwiftUI apps onto the iOS App Store. Students went from being new to development, to knowing everything it takes to build fully modern and stable iOS apps.",
+            "Provided teaching & tutoring assistance to 17 students to develop & publish 5 SwiftUI apps onto the iOS App Store. Students went from being new to development, to knowing what it takes to build fully modern and stable iOS apps.",
             "Helped students learn to use and work with GitHub, Trello, Cloud Databases, and what is and how to follow best app development & deployment practices."
         ]
     },
@@ -52,8 +53,9 @@ export const workExperience = [
         workTime:"Aug 2022 - Jun 2024",
         workType:"Part Time",
         points:[
-            "Tutored years of students across 4 classes with around 15 students per class about becoming developers. I helped students learn to code, create, publish, and manage apps, websites, databases, and more using popular industry-leading tools. That and being able to understand and follow best practices like agile workflow.",
-            "Students graduated with enough information ready to enter any development team with very little training needed."
+            "Tutored classes with around 15 students per class about becoming developers. I helped students learn to code, create, publish, and manage apps, websites, databases, and more using popular industry-leading tools. That and being able to understand and follow best practices like agile workflow.",
+            "Taught students concepts like debugging, coding languages differences & simularities, management, web development, app development, database development, best practices, and more while also following agile workflow.",
+            "Students graduated with enough information ready to enter any development team with very little to no training needed."
         ]
     },
 
